87190c344a0d9452d211d5588790d280619392b1
Luis Masuelli <luisfmasuelli@gmail.com>
2018-01-27 20:06:38 -0500

    Decoupling representation and z-ordering, and adding stuff

--- Assets/Scripts/WindRose/Behaviors/Objects/Sorted.cs
+++ Assets/Scripts/WindRose/Behaviors/Objects/Sorted.cs
@@ -10,44 +10,24 @@ namespace WindRose
         // We make no use of Snapped at all, but the behavior will
         //   automatically be called, and Positionable will be
         //   present anyway.
-        [RequireComponent(typeof(Snapped))]
-        [RequireComponent(typeof(Sorted))]
-        public class Represented : MonoBehaviour
+        [RequireComponent(typeof(SpriteRenderer))]
+        public class Sorted : MonoBehaviour
         {
             /**
-             * A represented object is a positionable object which can also display
-             *   a sprite (it is also a SpriteRenderer object). It will provide an
-             *   animation which will change on each frame.
+             * A represented object knows its z-order in the map.
              */
 
             private SpriteRenderer spriteRenderer;
             private Positionable positionable;
 
-            [SerializeField]
-            private Types.AnimationSpec defaultAnimation;
-
             public enum SubLayer { LOW, MIDDLE, HIGH }
             [SerializeField]
             private SubLayer subLayer = SubLayer.MIDDLE;
-
-            private Types.AnimationSpec currentAnimation;
-
-            public Types.AnimationSpec CurrentAnimation
-            {
-                get { return currentAnimation.Clone(); }
-                set { currentAnimation = value.Clone(); }
-            }
-
-            public void SetDefaultAnimation()
-            {
-                CurrentAnimation = defaultAnimation;
-            }
 
             void Start()
             {
                 spriteRenderer = GetComponent<SpriteRenderer>();
                 positionable = GetComponent<Positionable>();
-                SetDefaultAnimation();
             }
 
             void Update()
@@ -55,15 +35,11 @@ namespace WindRose
                 // We order the sprite
                 int sortingOffset = (int)(positionable.ParentMap.Width + positionable.ParentMap.Height) * ((int)(subLayer));
                 spriteRenderer.sortingOrder = sortingOffset + (int)(positionable.Yf * positionable.ParentMap.Width + positionable.Xf);
-                // And also set the current animation.
-                //   We are sure this animation will be non-empty.
-                currentAnimation.Thick();
-                spriteRenderer.sprite = currentAnimation.CurrentSprite;
             }
 
             void Pause(bool fullFreeze)
             {
-                enabled = !fullFreeze;
+                enabled = false;
             }
 
             void Resume()
